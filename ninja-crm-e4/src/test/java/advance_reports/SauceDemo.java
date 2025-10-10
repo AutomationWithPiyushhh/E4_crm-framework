a874eec0aad6ec79f110808ee01a8f8c85929eea
AutomationWithPiyush <automation.with.piyush@gmail.com>
2025-10-10 17:04:47 +0530

    Advance Reports

--- ninja-crm-e4/src/test/java/advance_reports/SauceDemo.java
+++ ninja-crm-e4/src/test/java/advance_reports/SauceDemo.java
@@ -2,6 +2,11 @@ package advance_reports;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
 
 import com.aventstack.extentreports.ExtentReports;
@@ -11,33 +16,62 @@ import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 import com.aventstack.extentreports.reporter.configuration.Theme;
 
 public class SauceDemo {
-	@Test
-	public void getReport() {
-		WebDriver driver = new ChromeDriver();
+	ExtentReports report;
+	WebDriver driver;
 
-		driver.get("https://www.saucedemo.com/v1/");
-
+	@BeforeSuite
+	public void repConfig() {
+		System.out.println("DB connectivity + Report configuration");
 //		report configuration
-		ExtentSparkReporter spark = new ExtentSparkReporter("./advance_reports/rep1.html");
+		ExtentSparkReporter spark = new ExtentSparkReporter("./advance_reports/rep2.html");
 		spark.config().setDocumentTitle("Sauce demo report");
 		spark.config().setReportName("First Report");
 		spark.config().setTheme(Theme.STANDARD);
 
-		ExtentReports report = new ExtentReports();
+		report = new ExtentReports();
 		report.attachReporter(spark);
 		report.setSystemInfo("username", "Vikas@123");
 		report.setSystemInfo("browser", "chrome");
 		report.setSystemInfo("window", "11");
+	}
 
+	@BeforeClass
+	public void openBro() {
+		driver = new ChromeDriver();
+	}
+
+	@Test
+	public void passing() {
 		ExtentTest test = report.createTest("getReport");
-		test.log(Status.INFO, "Information");
+		driver.get("https://www.saucedemo.com/v1/");
 		test.log(Status.PASS, "Passed");
-		test.log(Status.WARNING, "warning");
-		test.log(Status.SKIP, "skipped");
-		test.log(Status.FAIL, "failed");
+	}
 
-		report.flush();
+	@Test
+	public void failing() {
+		ExtentTest test = report.createTest("getReport");
+		driver.get("https://www.saucedemo.com/v1/");
+		test.log(Status.FAIL, "Failed");
+		Assert.assertFalse(true);
+		System.out.println("hey");
+	}
 
+	@Test(dependsOnMethods = "failing")
+	public void skipping() {
+		ExtentTest test = report.createTest("getReport");
+		driver.get("https://www.saucedemo.com/v1/");
+		test.log(Status.SKIP, "SKipped");
+	}
+
+	@AfterClass
+	public void closeBro() {
 		driver.quit();
+	}
+
+	@AfterSuite
+	public void repBackup() {
+		System.out.println("DB connectivity close + Report Backup");
+//		Report backup
+		report.flush();
 	}
 }
